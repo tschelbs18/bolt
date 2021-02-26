5c501fb9e6a01dd08e7202e4eb30fcddfb8798bc
AxiosDeminence <juhmer@att.net>
2021-02-25 22:01:37 -0800

    Refactored pastruns and routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,31 +1,9 @@
-var routes_data = {
-  routes: [
-    {"Name": "Bayshore Bikeway",
-    "Distance": "17.1 miles",
-    "Surface" : "Asphalt, Concrete"
-    },
-    {"Name": "Coastal Rail Trail",
-    "Distance": "4.7 miles",
-    "Surface" : "Asphalt, Concrete"
-    },
-    {"Name": "Mission Beach-Pacific Beach Boardwalk",
-    "Distance": "3 miles",
-    "Surface" : "Concrete"
-    },
-    {"Name": "Sorrento Valley Road Trail",
-    "Distance": "1 miles",
-    "Surface" : "Asphalt"
-    },
-    {"Name": "Rose Canyon Bicycle Path",
-    "Distance": "2.1 miles",
-    "Surface" : "Asphalt"
-    },
-  ]
-};
 /*
  * GET nearby routes page.
  */
 
+const jsonData = require('../public/js/data.json');
+
 exports.view = function(req, res){
-    res.render('routes', routes_data);
+    res.render('routes', { routes: jsonData.routes });
 }
