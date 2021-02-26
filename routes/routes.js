a9d8f978f797d5a07461af427677aa3b0ff00065
Ted Schelble <ted.h.schelble@gmail.com>
2021-02-25 21:38:19 -0800

    Added routes and past runs

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,31 @@
-var data = {};
+var routes_data = {
+  routes: [
+    {"Name": "Bayshore Bikeway",
+    "Distance": "17.1 miles",
+    "Surface" : "Asphalt, Concrete"
+    },
+    {"Name": "Coastal Rail Trail",
+    "Distance": "4.7 miles",
+    "Surface" : "Asphalt, Concrete"
+    },
+    {"Name": "Mission Beach-Pacific Beach Boardwalk",
+    "Distance": "3 miles",
+    "Surface" : "Concrete"
+    },
+    {"Name": "Sorrento Valley Road Trail",
+    "Distance": "1 miles",
+    "Surface" : "Asphalt"
+    },
+    {"Name": "Rose Canyon Bicycle Path",
+    "Distance": "2.1 miles",
+    "Surface" : "Asphalt"
+    },
+  ]
+};
 /*
  * GET nearby routes page.
  */
 
 exports.view = function(req, res){
-    res.render('routes', {data
-    });
+    res.render('routes', routes_data);
 }
